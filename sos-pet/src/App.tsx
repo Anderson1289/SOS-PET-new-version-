52b696cd9ecff3b76378471b5655023315029a57
Erika <erikaperciliano.developer@gmail.com>
2021-06-23 22:03:34 -0300

    add styked component

--- sos-pet/src/App.tsx
+++ sos-pet/src/App.tsx
@@ -1,8 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1 `
+  font-size: 64px;
+  color: yellow
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1>SOS PET</h1>
+      <Title>SOS PET</Title>
     </div>
   );
 }
