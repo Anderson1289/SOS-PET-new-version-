6cd8f9173023cca70ced9f2008049a2d57b59691
Erika <erikaperciliano.developer@gmail.com>
2021-06-29 00:54:28 -0300

    add screen of signup

--- sos-pet/src/App.tsx
+++ sos-pet/src/App.tsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
-import { Login } from './components/Login'
-import { Header } from './components/Header'
+import { Login } from './components/Login';
+import { Header } from './components/Header';
+import { SignUp } from './components/SignUp';
+
 import { GlobalStyle } from './styles/global';
+
 const Title = styled.h1 `
   font-size: 64px;
   color: yellow
@@ -10,7 +13,7 @@ const Title = styled.h1 `
 export function App() {
   return (
     <>
-      <Header/>
+      <SignUp/>
       <GlobalStyle/>
     </>
   );
