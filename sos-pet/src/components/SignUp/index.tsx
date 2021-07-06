41d4001b120175f34a8331e4377447cdc1145231
Erika <erikaperciliano.developer@gmail.com>
2021-07-05 23:27:47 -0300

    add value of form

--- sos-pet/src/components/SignUp/index.tsx
+++ sos-pet/src/components/SignUp/index.tsx
@@ -1,8 +1,32 @@
+import { FormEvent, useState } from 'react';
 import { MenuInitial } from '../MenuInitial';
 import { FaEnvelope, FaUser, FaLock, FaKey } from "react-icons/fa";
 import { Section, Body, UseTerm } from './styles';
 import Pets from '../../assets/pets.png'
+
 export function SignUp(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPasswordd] = useState('');
+    const [useTerm, setUseTerm] = useState('');
+
+    function handleCreateNewUser(event: FormEvent){
+        event.preventDefault();
+
+        console.log({
+            name,
+            email,
+            password,
+            confirmPassword,
+            useTerm
+        })
+    }
+
+    function teste(){
+        console.log('Fui clicado!')
+    }
+
     return (
         <> 
             <MenuInitial/>
@@ -17,33 +41,63 @@ export function SignUp(){
 
                                     <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Cadastre-se</p>
 
-                                    <form className="mx-1 mx-md-4">
+                                    <form className="mx-1 mx-md-4" onSubmit={handleCreateNewUser}>
                                         <div className="d-flex flex-row align-items-center mb-4">
                                             <FaUser className="fas fa-user fa-lg me-3 fa-fw"/>
-                                            <input type="text" className="form-control" placeholder="Nome" required/>
+                                            <input 
+                                                type="text" 
+                                                className="form-control" 
+                                                placeholder="Nome" 
+                                                value={name} 
+                                                onChange = {event => setName(event.target.value)} 
+                                                required
+                                            />
                                         </div>
 
                                         <div className="d-flex flex-row align-items-center mb-4">
                                             <FaEnvelope className="fas fa-envelope fa-lg me-3 fa-fw"/>
-                                        <input type="email" className="form-control" placeholder="E-mail" required/>
+                                        <input 
+                                            type="email" 
+                                            className="form-control" 
+                                            placeholder="E-mail" 
+                                            value={email} 
+                                            onChange = {event => setEmail(event.target.value)} 
+                                            required
+                                        />
                                         </div>
 
                                         <div className="d-flex flex-row align-items-center mb-4">
                                             <FaLock className="fas fa-lock fa-lg me-3 fa-fw"/>
-                                            <input type="password" className="form-control" placeholder="Senha" required/>
+                                            <input 
+                                                type="password" 
+                                                className="form-control" 
+                                                placeholder="Senha" 
+                                                value={password} 
+                                                onChange = {event => setPassword(event.target.value)} 
+                                                required
+                                            />
                                         </div>
 
                                         <div className="d-flex flex-row align-items-center mb-4">
                                             <FaKey className="fas fa-key fa-lg me-3 fa-fw"/>
-                                                <input className="password form-control" placeholder="Repita sua senha" required/>
+                                                <input 
+                                                    className="password form-control" 
+                                                    type="password"  
+                                                    placeholder="Repita sua senha" 
+                                                    value={confirmPassword} 
+                                                    onChange = {event => setConfirmPasswordd(event.target.value)} 
+                                                    required
+                                                />
                                         </div>
 
                                         <UseTerm className="form-check d-flex justify-content-center mb-5">
                                         <input
                                             className="form-check-input me-2"
                                             type="checkbox"
-                                            value=""
                                             id="form2Example3c"
+                                            value = {useTerm} 
+                                            onChange = {event => setUseTerm(event.target.value)} 
+                                            required
                                         />
                                         <label className="form-check-label">
                                             Concordar com todas as declarações <a href="#!" className="useTerm">nos Termos de serviço</a>
@@ -58,7 +112,7 @@ export function SignUp(){
 
                                 </div>
                                 <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                                    <img src={Pets} className="img-fluid useTerm" alt="Sample image"/>
+                                    <img src={Pets} className="img-fluid useTerm" alt="Sample"/>
                                 </div>
                                 </div>
                             </div>
